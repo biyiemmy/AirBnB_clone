8d1020639fa409fa378e07b6edbd22c458a6d26f
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2022-11-28 10:00:11 +0100

    task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -39,26 +39,6 @@ class FileStorage():
             # json.dump(object_dict, jsonfile)
             jsonfile.write(json.dumps(object_dict))
 
-    # def classes(self):
-    #     """Returns a dictionary of valid classes and their references."""
-    #     from models.base_model import BaseModel
-    #     from models.user import User
-    #     from models.state import State
-    #     from models.city import City
-    #     from models.amenity import Amenity
-    #     from models.place import Place
-    #     from models.review import Review
-
-    #     classes = {"BaseModel": BaseModel,
-    #                "User": User,
-    #                "State": State,
-    #                "City": City,
-    #                "Amenity": Amenity,
-    #                "Place": Place,
-    #                "Review": Review
-    #                }
-    #     return classes
-
     def reload(self):
         """Reloads the stored objects"""
         if os.path.exists(FileStorage.__file_path):
@@ -71,41 +51,3 @@ class FileStorage():
                 class_name = dictionary['__class__']
                 final_dict[id] = self.classes()[class_name](**dictionary)
             FileStorage.__objects = final_dict
-
-    # def attributes(self):
-    #     """Returns the valid attributes and their types for classname."""
-    #     attributes = {
-    #         "BaseModel":
-    #                  {"id": str,
-    #                   "created_at": datetime.datetime,
-    #                   "updated_at": datetime.datetime},
-    #         "User":
-    #                  {"email": str,
-    #                   "password": str,
-    #                   "first_name": str,
-    #                   "last_name": str},
-    #         "State":
-    #                  {"name": str},
-    #         "City":
-    #                  {"state_id": str,
-    #                   "name": str},
-    #         "Amenity":
-    #                  {"name": str},
-    #         "Place":
-    #                  {"city_id": str,
-    #                   "user_id": str,
-    #                   "name": str,
-    #                   "description": str,
-    #                   "number_rooms": int,
-    #                   "number_bathrooms": int,
-    #                   "max_guest": int,
-    #                   "price_by_night": int,
-    #                   "latitude": float,
-    #                   "longitude": float,
-    #                   "amenity_ids": list},
-    #         "Review":
-    #         {"place_id": str,
-    #                      "user_id": str,
-    #                      "text": str}
-    #     }
-    #     return attributes
